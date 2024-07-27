4dc800e82fe2da52e47503eaf69055ec00706744
golib9191 <octa.g91@gmail.com>
2024-07-27 18:29:20 +0500

    Js8a

---
+++ index.js
@@ -0,0 +1,115 @@
+'use strict';
+
+// const basketboll={
+//     narxi: 2095,
+// }
+// console.log(basketboll.narxi+500);
+// basketboll["Delivery"] = '3-days'
+// console.log(basketboll);
+//8d
+//
+// const mahsulot1 = {
+//     nomi: "basketboll",
+//     narxi: 2095
+// }
+// const mahsulot2 = {
+//     nomi: "football",
+//     narxi: 3095
+// }
+//
+// function mahuslotTaqqoslash(mahsulot1, mahsulot2) {
+//     if (mahsulot1.narxi < mahsulot2.narxi) {
+//         console.log(mahsulot1.nomi);
+//     }else{
+//         console.log(mahsulot2.narxi);
+//     }
+// }
+// mahuslotTaqqoslash(mahsulot1, mahsulot2);
+
+//8e
+//
+// const mahsulot1 = {
+//     nomi: "basketboll",
+//     narxi: 2095
+// }
+// const mahsulot2 = {
+//     nomi: "football",
+//     narxi: 3095
+// }
+// function turiniTaqqoslash(mahsulot1, mahsulot2) {
+//     if(mahsulot1.narxi === mahsulot2.narxi){
+//         console.log(true)
+//     }else{
+//         console.log(false)
+//     }
+// }
+// turiniTaqqoslash(mahsulot1, mahsulot2);
+
+
+// //8f
+// let a = 'Good Morning'
+// console.log(a.toUpperCase())
+// console.log(a.toLowerCase())
+//
+// 01
+// const rasta1 = {
+//     tarvuz: "15 kg lik tarvuz",
+//     narxi: 15000
+// }
+// const rasta2 = {
+//     tarvuz: "15 kglik tarvuz",
+//     narxi: 17000
+// }
+// function arzonNarxda (rasta1,rasta2){
+//     if (rasta1.narxi < rasta2.narxi) {
+//         console.log(rasta1.tarvuz);
+//     }else{
+//         console.log(rasta2.tarvuz);
+//     }
+// }
+// arzonNarxda(rasta1, rasta2)
+
+//02
+
+// const komp1 = {
+//     name: 'Asus',
+//     core: 'i7',
+//     ram: 32
+// }
+// const komp2 = {
+//     name: 'Acer',
+//     core: "i7",
+//     ram: 16
+// }
+// function kuchliKomp(komp1, komp2){
+//     if (komp1.ram < komp2.ram) {
+//         console.log(komp2.name)
+//     }else{
+//         console.log(komp1.name)
+//     }
+// }
+// kuchliKomp(komp1,komp2);
+
+//03
+//
+// const komp1 = {
+//     name: 'Asus',
+//     core: 'i5',
+//     ram: 32
+// }
+// const komp2 = {
+//     name: 'Acer',
+//     core: "i5",
+//     ram: 16
+// }
+// function kuchliKomp(komp1, komp2){
+//     if (komp1.core === komp2.core) {
+//         console.log(komp1.name, 'va', komp2.name, 'parametrlari bir xil');
+//     }else{
+//         console.log('komp parametrlari bir xil emas')
+//     }
+// }
+// kuchliKomp(komp1,komp2);
+
+
+
